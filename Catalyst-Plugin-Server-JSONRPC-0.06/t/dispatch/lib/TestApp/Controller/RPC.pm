3ab9a354bfc8ca390f3371f724f88d19c892ed42
Jose Luis Martinez <jlmartinez@capside.com>
2010-07-08 21:04:52 +0200

    File missing in test suite

--- Catalyst-Plugin-Server-JSONRPC-0.06/t/dispatch/lib/TestApp/Controller/RPC.pm
+++ Catalyst-Plugin-Server-JSONRPC-0.06/t/dispatch/lib/TestApp/Controller/RPC.pm
@@ -3,7 +3,7 @@ package TestApp::Controller::RPC;
 use base 'Catalyst::Controller';
 
 ### accept every jsonrpc request here
-sub my_dispatcher : JSONRPCRegex('^.$') {
+sub my_dispatcher : JSONRPCRegex('.') {
     my( $self, $c ) = @_;
     ### return the name of the method you called
     $c->stash->{'jsonrpc'} = $c->request->jsonrpc->method;
